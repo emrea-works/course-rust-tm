1aa42a0e7c0414e5d7f300c6120d6ac66f7be441
emrea-works <gelisim.odakli@gmail.com>
2022-05-18 23:19:00 -0300

    positional arguments

--- src/print.rs
+++ src/print.rs
@@ -1,7 +1,13 @@
 pub fn run() {
-    // print console
-    println!("Hello from print.rs file");   
+    // Print Console
+    println!("Hello from print.rs file");
 
-    // Basic formatting
+    // Basic Formatting
     println!("{} is from {}", "Brad", "Mass");
+
+    // Positional Arguments
+    println!(
+        "{0} is from {1} and {0} likes to {2}",
+        "Brad", "Mass", "code"
+    );
 }
