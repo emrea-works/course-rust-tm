fdb762abf180ed64722688a645c1aa2d2355b6d5
Emre Akyüz <gelisim.odakli@gmail.com>
2022-07-27 23:42:20 +0100

    structs: Person struct and constructor

--- src/structs.rs
+++ src/structs.rs
@@ -5,7 +5,22 @@
 // }
 
 // Tuple Struct
-struct Color(u8, u8, u8);
+// struct Color(u8, u8, u8);
+
+struct Person {
+    first_name: String,
+    last_name: String,
+}
+
+impl Person {
+    // Construct person
+    fn new(first: &str, last: &str) -> Person {
+        Person {
+            first_name: first.to_string(),
+            last_name: last.to_string(),
+        }
+    }
+}
 
 pub fn run() {
     // let mut c = Color {
@@ -18,9 +33,12 @@ pub fn run() {
 
     // println!("Color: {} {} {}", c.red, c.green, c.blue);
 
-    let mut c = Color(150, 0, 0);
+    // let mut c = Color(150, 0, 0);
 
-    c.2 = 99;
+    // c.2 = 99;
 
-    println!("Color: {} {} {}", c.0, c.1, c.2);
+    // println!("Color: {} {} {}", c.0, c.1, c.2);
+
+    let mut p = Person::new("John", "Doe");
+    println!("Person: {} {}", p.first_name, p.last_name);
 }
