878c9c98e3be06487c73b1c7c57d10b5e3bb80ae
Emre Akyüz <gelisim.odakli@gmail.com>
2022-07-28 00:27:49 +0100

    structs: Person add a setting method

--- src/structs.rs
+++ src/structs.rs
@@ -25,6 +25,11 @@ impl Person {
     fn full_name(&self) -> String {
         format!("{} {}", self.first_name, self.last_name)
     }
+
+    // Set last name
+    fn set_last_name(&mut self, last: &str) {
+        self.last_name = last.to_string();
+    }
 }
 
 pub fn run() {
@@ -44,7 +49,9 @@ pub fn run() {
 
     // println!("Color: {} {} {}", c.0, c.1, c.2);
 
-    let mut p = Person::new("John", "Doe");
+    let mut p = Person::new("Mary", "Doe");
     // println!("Person: {} {}", p.first_name, p.last_name);
+    println!("Person Full Name: {}", p.full_name());
+    p.set_last_name("Williams");
     println!("Person Full Name: {}", p.full_name());
 }
