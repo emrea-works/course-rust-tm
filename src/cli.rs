70c16863149217ef908518b35fcf335d5f1c743a
emrea-works <gelisim.odakli@gmail.com>
2022-08-12 11:34:52 -0300

    cli example: switch mech between commands as args, cloning

--- src/cli.rs
+++ src/cli.rs
@@ -1,7 +1,23 @@
 use std::env;
 
 pub fn run() {
+    /* args: variable is a vector data (infinite length array)
+     * that get assigned by env library and its methods which
+     * will be called at execution time, which collects the
+     * arguments that were given, just like as in array's push
+     */
     let args: Vec<String> = env::args().collect();
+    // println!("Args: {:?}", args);
 
-    println!("Args: {:?}", args);
+    let command = args[1].clone();
+    let name = "Brad";
+    let status = "100%";
+
+    if command == "hello" {
+        println!("Hi {}, how are you?", name);
+    } else if command == "status" {
+        println!("Status is {}", status);
+    } else {
+        println!("That is not a valid command");
+    }
 }
